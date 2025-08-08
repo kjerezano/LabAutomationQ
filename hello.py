0777a791e345cde4727efe521a8649e213857435
kjerezano <kevinjerezano@outlook.com>
2025-08-08 08:53:19 -0500

    Austin updates 2

---
+++ hello.py
@@ -0,0 +1,16 @@
+#Create a variable to store a name
+name = "Austin"
+#Variable to input
+Celsius = float(input())
+
+print("Hello, "+name+"!")
+print(f"Hello, {name}! My favorite number is 3{3*1}")
+
+print("Welcome to Python")
+
+
+print (f"{Celsius} is {Celsius * 1.8 + 32} in Farenheit")
+
+
+
+
